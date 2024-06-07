e73d74c0c6af6d5afc29ccd4599a5752107be0c3
Youri Westerman <y.westerman@amsterdam.nl>
2024-06-07 11:32:33 +0200

    Crude skeleton

---
+++ plugfs/filesystem.py
@@ -0,0 +1,44 @@
+from abc import ABCMeta, abstractmethod
+from collections.abc import Iterator
+from typing import Collection, final
+
+
+class FilesystemItem:
+    def get_path(self) -> str: ...
+    def get_name(self) -> str: ...
+
+
+class DirectoryListing(Collection[FilesystemItem]):
+    def __len__(self) -> int:
+        pass
+
+    def __iter__(self) -> Iterator[FilesystemItem]:
+        pass
+
+    def __contains__(self, __x: object) -> bool:
+        pass
+
+
+class Directory(FilesystemItem): ...
+
+
+class File(FilesystemItem):
+    def get_size(self) -> int: ...
+
+    # TODO: read, write, metadata, etc
+
+
+class Adapter(metaclass=ABCMeta):
+    @abstractmethod
+    async def list(self, path: str) -> list[str]: ...
+
+
+@final
+class Filesystem:
+    _adapter: Adapter
+
+    def __init__(self, adapter: Adapter):
+        self._adapter = adapter
+
+    async def list(self, path: str) -> list[str]:
+        return await self._adapter.list(path)
