6a751ae0af5d2107f50ad688ee0682a5bfb77017
Youri Westerman <y.westerman@amsterdam.nl>
2024-06-10 11:26:12 +0200

    Improve test

--- tests/test_local.py
+++ tests/test_local.py
@@ -1,12 +1,26 @@
+from os import path
+
 import pytest
 
-from plugfs.filesystem import Filesystem
+from plugfs.filesystem import Directory, File, Filesystem
 from plugfs.local import LocalAdapter
 
 
 @pytest.mark.asyncio
 async def test_list() -> None:
     filesystem = Filesystem(LocalAdapter())
-    items = await filesystem.list("/")
-    for item in items:
-        print(item)
+    items = await filesystem.list(path.join(path.dirname(__file__), "resources"))
+
+    assert len(items) == 2
+
+    assert isinstance(items[0], File)
+    assert items[0].name == "1mb.bin"
+    assert items[0].path == path.join(
+        path.abspath(path.dirname(__file__)), "resources", "1mb.bin"
+    )
+
+    assert isinstance(items[1], Directory)
+    assert items[1].name == "directory"
+    assert items[1].path == path.join(
+        path.abspath(path.dirname(__file__)), "resources", "directory"
+    )
