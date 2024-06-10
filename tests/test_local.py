7ecd6e2495ba9b58852a93ced8c8bf8bc28bf5fa
Youri Westerman <y.westerman@amsterdam.nl>
2024-06-10 14:03:57 +0200

    Test reading local files

--- tests/test_local.py
+++ tests/test_local.py
@@ -35,3 +35,25 @@ class TestLocalAdapter:
             str(exception_info.value)
             == "Failed to retrieve directory listing for '/this/path/does/not/exist'!"
         )
+
+    @pytest.mark.asyncio
+    async def test_read(self) -> None:
+        adapter = LocalAdapter()
+
+        data = await adapter.read(
+            path.join(path.abspath(path.dirname(__file__)), "resources", "1mb.bin")
+        )
+
+        assert len(data) == 1048576
+
+    @pytest.mark.asyncio
+    async def test_read_non_existing(self) -> None:
+        adapter = LocalAdapter()
+
+        with pytest.raises(NotFoundException) as exception_info:
+            await adapter.read("/this/path/does/not/exist")
+
+        assert (
+            str(exception_info.value)
+            == "Failed to find file '/this/path/does/not/exist'!"
+        )
