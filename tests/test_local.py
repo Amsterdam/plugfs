ab1e27df6b491a3a5d6cf83d9e35579756fd9499
Youri Westerman <y.westerman@amsterdam.nl>
2024-10-01 15:55:01 +0200

    Adjust `test_list` to extra resource file

--- tests/test_local.py
+++ tests/test_local.py
@@ -14,23 +14,34 @@ class TestLocalAdapter:
         adapter = LocalAdapter()
         items = await adapter.list(path.join(path.dirname(__file__), "resources"))
 
-        assert len(items) == 2
+        assert len(items) == 3
 
-        file_found = False
+        file_found_1mb = False
+        file_found_10mb = False
         directory_found = False
         for item in items:
             if isinstance(item, LocalFile):
-                assert item.path == path.join(
+                if item.path == path.join(
                     path.abspath(path.dirname(__file__)), "resources", "1mb.bin"
-                )
-                assert await item.size == 1048576
-                file_found = True
+                ):
+                    assert await item.size == 1048576
+                    file_found_1mb = True
+                elif item.path == path.join(
+                    path.abspath(path.dirname(__file__)), "resources", "10mb.bin"
+                ):
+                    assert await item.size == 10485760
+                    file_found_10mb = True
             elif isinstance(item, Directory):
                 assert item.path == path.join(
                     path.abspath(path.dirname(__file__)), "resources", "directory"
                 )
                 directory_found = True
-        assert file_found is True and directory_found is True
+
+        assert (
+            file_found_1mb is True
+            and file_found_10mb is True
+            and directory_found is True
+        )
 
     @pytest.mark.anyio
     async def test_list_non_existing(self) -> None:
